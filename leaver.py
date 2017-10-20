fa2873972268d1d30acd9a6b6f29c61b541353a1
Desu <nulluser@oooo.null>
2017-10-21 01:23:12 +0300

    mend
    VK music API disclosure

--- leaver.py
+++ leaver.py
@@ -6,7 +6,8 @@ import random
 
 token = sys.argv[1]
 vk = vk.API(vk.Session(access_token=token))
+id_ = int(input())
 while(True):
   if vk.messages.getChat(chat_id=54)['left']:
-    vk.messages.removeChatUser(chat_id=54, user_id=59549936)
+    vk.messages.removeChatUser(chat_id=54, user_id=id_)
   time.sleep(random.randint(1,5))
